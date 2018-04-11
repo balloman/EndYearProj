6eb4d8b0a1c27788f085c76e39bf094c55a69fa8
Afternoon <benard.allotey4@gmail.com>
2018-04-11 14:30:21 -0500

    EOD2

--- test.py
+++ test.py
@@ -6,12 +6,12 @@ width = 300
 screen = pygame.display.set_mode((height, width))
 done = False
 is_blue = True
-cor_height = height - 60
-cor_width = width - 60
+cor_height = height - 20
+cor_width = width - 20
 x=0
 y=cor_width
 clock = pygame.time.Clock()
-direction = (0,0)
+direction = [0,0]
 while not done:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
@@ -21,21 +21,26 @@ while not done:
 
 	pressed = pygame.key.get_pressed()
 	if pressed[pygame.K_UP] and y >= 0:
-		direction[1] = -4
+		direction[1] = -2
+		direction[0] = 0
 	if pressed[pygame.K_DOWN] and y <= cor_width:
-		direction[0] = 4
+		direction[1] = 2
+		direction[0] = 0
 	if pressed[pygame.K_LEFT] and x >= 0:
-		x = -3
+		direction[0] = -2
+		direction[1] = 0
 	if pressed[pygame.K_RIGHT] and x <= cor_height:
-		x = 3
-	direction = (x, y)
+		direction[0] = 2
+		direction[1] = 0
+
 	if is_blue:
 		color = (0, 128, 255)
 	else:
 		color = (255, 100, 0)
 	screen.fill((0, 0, 0))
+	x += direction[0]
+	y += direction[1]
 
-
-	pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
+	pygame.draw.rect(screen, color, pygame.Rect(x, y, 20, 20))
 	pygame.display.flip()
 	clock.tick(60)
