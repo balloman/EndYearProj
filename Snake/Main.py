bbd6332dff0dd94be74ca4ab38f9668dfff0be8d
Afternoon <benard.allotey4@gmail.com>
2018-04-13 15:53:46 -0500

    Added a system for the game to use classes instead of sloppy

--- Snake/Main.py
+++ Snake/Main.py
@@ -1,12 +1,13 @@
+"""This is a much neater and more modular version of test.py with classes"""
+
 import pygame
 from random import randint
 
-HEIGHT = 1080
-WIDTH = 1920
+HEIGHT = 540
+WIDTH = 960
 
 
-def control(speed):
-    direction = [0,0]
+def control(speed, direction=[0, 0]):
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_UP]:
         direction[1] = -speed
@@ -22,11 +23,15 @@ def control(speed):
         direction[1] = 0
     return direction
 
+
 class Game(object):
     clock = pygame.time.Clock()
 
-    def __init__(self, width, height, mode, fps=60):
-        self.screen = pygame.display.set_mode((width, height), mode)
+    def __init__(self, width, height, mode=None, fps=60):
+        if mode is None:
+            self.screen = pygame.display.set_mode((width, height))
+        else:
+            self.screen = pygame.display.set_mode((width, height), mode)
         self.cor_width = width - 60
         self.cor_height = height - 60
         self.fps = fps
@@ -37,21 +42,77 @@ class Game(object):
     def setDisplay(self, width, height, mode):
         self.screen = pygame.display.set_mode((width, height), mode)
 
+    def tick(self):
+        self.clock.tick(self.fps)
 
-class Player(object):
 
-    def __init__(self, screen, rect, color=(0, 0, 255)):
-        self.screen = screen
+class Cube(object):
+    def __init__(self, game: Game, color=(255, 0, 0), position=None):
+        self.game = game
+        self.surface = game.screen
         self.color = color
-        self.rect = rect
-        self.direction = [0,0]
+        if position is None:
+            position = [randint(0, game.cor_width/60), randint(0, game.cor_height/60)]
+        self.position = position
+        self.rect = pygame.Rect(position[0], position[1], 60, 60)
 
-    def
+    def drawCube(self):
+        pygame.draw.rect(self.surface, self.color, self.rect)
 
+
+class Player(Cube):
+    def __init__(self, game: Game, size: tuple, speed=60):
+        self.x = 0
+        self.y = game.cor_height
+        self.position = [self.x, self.y]
+        self.color = (0, 0, 255)
+        super().__init__(game, self.color, self.position)
+        self.size = size
+        self.speed = speed
+
+    def move(self):
+        direction = control(60)
+        if self.x <= 0 and direction[0] == -self.speed:
+            self.x += 0
+        else:
+            self.x += direction[0]
+        if self.y <= 0 and direction[1] == -self.speed:
+            self.y += 0
+        else:
+            self.y += direction[1]
+        self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
+
+    def eat(self):
+        pass
+
+
+def quitCheck(player: Player):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            return True
+        else:
+            return False
+    if player.x > player.game.cor_width:
+        return True
+    if player.y > player.game.cor_height:
+        return True
+    return False
 
 
 def start():
-    game = Game(WIDTH, HEIGHT, pygame.FULLSCREEN)
+    game = Game(WIDTH, HEIGHT)
     game.setFPS(10)
-    while True:
-        game.clock.tick()
+    player1 = Player(game, (60, 60))
+    game.screen.fill((0, 0, 0))
+    done = False
+    while not done:
+        done = quitCheck(player1)
+        player1.move()
+        player1.drawCube()
+        pygame.display.flip()
+        game.screen.fill((0, 0, 0))
+        game.tick()
+
+
+start()
+
